f7ad9171a6af5545d262de00cf28b21e605c4102
Siva <siva.axexa@gmail.com>
2026-07-15 22:50:20 +0530

    init: base project with core dependenies

---
+++ src/main/java/com/backendatlas/BackendAtlasApplication.java
@@ -0,0 +1,13 @@
+package com.backendatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAtlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAtlasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendatlas/BackendAtlasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendatlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAtlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
